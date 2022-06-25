3ba82d84800113210c2fca3d1ae1feb0a589dfeb
KenShyNoShana <ReneAslanov02@gmail.com>
2022-06-25 16:43:36 +0200

    add test to Image-resize function

--- __tests__/controllers/ImageController.test.ts
+++ __tests__/controllers/ImageController.test.ts
@@ -1,5 +1,7 @@
 import request from "supertest";
 import app from "../../src/app";
+import ImgController from "../../src/controllers/ImageController";
+import { mockRequest, mockResponse } from "mock-req-res";
 
 describe("ImgController", () => {
 	it("should return a 404 error if the image does not exist ", async () => {
@@ -20,5 +22,25 @@ describe("ImgController", () => {
 	it("should return a 200 status code if proper data is provided", async () => {
 		const testCall = await request(app).get("/resizeImage?imageName=cute-Panda&width=200&height=200");
 		expect(testCall.status).toEqual(200);
+	});
+
+	it("should call the function if proper data is provided", async () => {
+		jest.spyOn(ImgController, "resizeImage");
+
+		const req = mockRequest({
+			query: {
+				imageName: "cute-Panda",
+				width: "700",
+				height: "700",
+			},
+		});
+		const res = mockResponse();
+		ImgController.resizeImage(req, res);
+
+		expect(ImgController.resizeImage).toHaveBeenCalled();
+		expect(ImgController.resizeImage).toBeCalledWith(req, res);
+		expect(async () => {
+			ImgController.resizeImage(req, res);
+		}).not.toThrow();
 	});
 });
