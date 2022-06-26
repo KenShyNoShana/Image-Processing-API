48040bbbab233f970d85eaf34c1afb8a189c3633
KenShyNoShana <ReneAslanov02@gmail.com>
2022-06-26 12:26:09 +0200

    refactor: image-processing is now a seperate function

--- src/controllers/ImageController.ts
+++ src/controllers/ImageController.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
-import sharp from "sharp";
 import * as fs from "fs";
 import * as path from "path";
+import ImageResize from "../services/ImageResize";
 
 /* THIS CONTROLLER WILL PARSE THE REQUEST AND CALL THE RIGHT SERVICE ACTION*/
 export class ImgController {
@@ -17,7 +17,7 @@ export class ImgController {
 			);
 		}
 
-		fs.readFile(`./pictures/original/${imageName}.jpg`, (error, data): object | undefined | void => {
+		fs.readFile(`./pictures/original/${imageName}.jpg`, (error): object | undefined | void => {
 			if (error) {
 				res.status(404);
 				return res.send(
@@ -32,7 +32,7 @@ export class ImgController {
 
 			if (parseInt(width) <= 0 || parseInt(height) <= 0) {
 				res.status(404);
-				return res.send("width and height values need to be positive integers");
+				return res.send("width and height values need to be positive integers greater than 0");
 			}
 
 			const imageFiles = fs.readdirSync(path.join(__dirname, `../../pictures/resized`));
@@ -43,24 +43,16 @@ export class ImgController {
 				});
 			}
 
-			sharp(data)
-				.resize(parseInt(width), parseInt(height))
-				.toFormat("jpg")
-				// using .toFile instead of .toBuffer and filesystem makes the code cleaner and easier to understand
-				.toFile(
-					`${path.join(__dirname, `../../pictures/resized/${imageName}-resized-${width}x${height}.jpg`)}`,
-					(error: object): void => {
-						if (error) {
-							res.status(500);
-							res.send("A Server-side error has occured, please try again");
-						}
-
-						res.status(200);
-						return res.sendFile(`./pictures/resized/${imageName}-resized-${width}x${height}.jpg`, {
-							root: path.join(__dirname, "../.."),
-						});
-					},
-				);
+			ImageResize(width, height, imageName).then((actualImage) => {
+				if (!actualImage) {
+					res.status(500);
+					res.send("A Server-side error has occured, please try again");
+				}
+				res.status(200);
+				return res.sendFile(`./pictures/resized/${imageName}-resized-${width}x${height}.jpg`, {
+					root: path.join(__dirname, "../.."),
+				});
+			});
 		});
 	}
 }
